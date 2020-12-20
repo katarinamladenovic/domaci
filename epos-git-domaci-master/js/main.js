0725e1de6b7d5e3ded49fc6b43323e14987d441c
katarinamladenovic <76404548+katarinamladenovic@users.noreply.github.com>
2020-12-20 20:56:52 +0100

    domaci

---
+++ epos-git-domaci-master/js/main.js
@@ -0,0 +1,25 @@
+function darkMode() {
+    let element1 = document.body;
+    var element2 = document.getElementById('1st');
+    var element3 = document.getElementById('2nd');
+    var element4 = document.getElementById('3rd');
+    var element5 = document.getElementById('1sth2');
+    var element6 = document.getElementById('2ndh2');
+    var element7 = document.getElementById('3rdh2');
+    var element8 = document.getElementById('glavni');
+    var element9 = document.getElementById('header1');
+    var element10 = document.getElementById('header2');
+    element8.style.background="#000";
+    element1.style.background="#000";
+    element1.style.color="#fff";
+    console.log(element2);
+    element2.style.background="#000";
+    element3.style.background="#000";
+    element4.style.background="#000";
+    element5.style.color="#fff";
+    element6.style.color="#fff";
+    element7.style.color="#fff";
+    element9.style.color="#fff";
+    element10.style.color="#fff";
+ }
+ 
